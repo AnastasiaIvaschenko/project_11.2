6b5a2ffcc8c8d92f552540480a2335f7cdbd4628
AnastasiaIvaschenko <anivasch@mail.ru>
2023-03-15 10:48:16 +0300

    added first_cap_letters() func with dockstring to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,5 @@
 def capital_letters(text):
     return text.upper()
+def fist_cap_letters(text):
+    '''функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции'''
+    return text.title()
